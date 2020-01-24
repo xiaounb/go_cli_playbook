e7d222106b110becad2d7b6bcc32ea29b326d6f3
Hong Xiao <jeffhongxiao@gmail.com>
2020-01-24 10:52:08 -0400

    use bufio ReadString

--- test/check.go
+++ test/check.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
 
+func main() {
+	f, err := os.Open("../src/command.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	r := bufio.NewReader(f)
+	for {
+		s, err := r.ReadString('\n') // TODO: cross-platform
+		if err != nil {
+			break
+		}
+
+		fmt.Println(s)
+	}
 }
