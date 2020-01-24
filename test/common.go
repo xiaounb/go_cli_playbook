7743cf765627d66dafeeff97cb6b6f8f79a2752d
Hong Xiao <jeffhongxiao@gmail.com>
2020-01-24 13:08:20 -0400

    extract OpenFileAndFindString into common.go

---
+++ test/common.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strings"
+)
+
+// OpenFileAndFindString opens a file and return if the given string is found or not
+func OpenFileAndFindString(filename string, expected string) bool {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		trimmed := strings.Trim(t, " ")
+		if trimmed == "" {
+			continue
+		}
+		if trimmed == expected {
+			return true
+		}
+	}
+
+	return false
+}
--- test/module1_test.go
+++ test/module1_test.go
@@ -1,45 +1,13 @@
 package main
 
 import (
-	"bufio"
-	"log"
-	"os"
-	"strings"
 	"testing"
 )
 
 func TestNakedCall(t *testing.T) {
-	found := openFileAndFindString("go env")
+	found := OpenFileAndFindString("../src/module1.txt", "go env")
 
-	if found == false {
-		t.Errorf("Not found 'go env' in the command file")
+	if !found {
+		t.Errorf("'go env' not found in the command")
 	}
-}
-
-func findNthCommand() string {
-	return "Got"
-}
-
-func openFileAndFindString(expected string) bool {
-	f, err := os.Open("../src/module1.txt")
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	scanner.Split(bufio.ScanLines)
-
-	for scanner.Scan() {
-		t := scanner.Text()
-		trimmed := strings.Trim(t, " ")
-		if trimmed == "" {
-			continue
-		}
-		if trimmed == expected {
-			return true
-		}
-	}
-
-	return false
 }
