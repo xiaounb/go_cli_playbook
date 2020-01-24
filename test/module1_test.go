4c5fece896d2c87712a76b50f0885082ff900f4d
Hong Xiao <jeffhongxiao@gmail.com>
2020-01-24 13:11:47 -0400

    test for 'go env -json'

--- test/module1_test.go
+++ test/module1_test.go
@@ -11,3 +11,11 @@ func TestNakedCall(t *testing.T) {
 		t.Errorf("'go env' not found in the command")
 	}
 }
+
+func TestJsonOutput(t *testing.T) {
+	found := OpenFileAndFindString("../src/module1.txt", "go env -json")
+
+	if !found {
+		t.Errorf("'go env' should be called with the '-json' flag")
+	}
+}
