a48dcfaa37632a517c6f4fce21311b596e5fe8ec
Hong Xiao <jeffhongxiao@gmail.com>
2020-01-25 06:28:22 -0400

    add TestModule2DefaultParams()

--- test/module2_test.go
+++ test/module2_test.go
@@ -11,3 +11,11 @@ func TestModule2NakedCall(t *testing.T) {
 		t.Errorf("'gofmt <filename>' not found")
 	}
 }
+
+func TestModule2DefaultParams(t *testing.T) {
+	found := OpenFileAndFindString("../src/module2.txt", "gofmt -l -w .")
+
+	if !found {
+		t.Errorf("See 'go help fmt' for more details")
+	}
+}
