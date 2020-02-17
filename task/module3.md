21be6c33e0136c0ef3cd49d3b28d04dfd8607db6
Hong Xiao <jeffhongxiao@gmail.com>
2020-02-17 19:08:51 -0400

    module3 (go get) initial commit

---
+++ task/module3.md
@@ -0,0 +1,14 @@
+## Purpose of this module
+This module will teach you how to use `go get` to download and install packages.
+We will learn the `go get` command and what needs to be provided to it.
+
+
+## Verify locally
+To test this module locally:
+* Open up a terminal at the project root
+* Run command `cd test` to change to the `test` directory
+* Run command `go test -run Module3` to run all tests for module 3, or
+* Run command `go test -v -run Module3` to run all tests for module 3 with verbose information
+
+## Task 1
+TODO
--- test/common.go
+++ test/common.go
@@ -33,6 +33,34 @@ func OpenFileAndCountLines(filename string) int {
 	return number
 }
 
+// OpenFileAndFindNthString opens a file, look for Nth string splitted by a space, and return if given expected string is found or not
+func OpenFileAndFindNthString(filename string, nth int, expected string) bool {
+	f, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+	scanner.Split(bufio.ScanLines)
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		trimmed := strings.Trim(t, " ")
+		if trimmed == "" {
+			continue
+		}
+
+		// matching logic
+		ss := strings.Split(trimmed, " ")
+		if ss[nth] == expected {
+			return true
+		}
+	}
+
+	return false
+}
+
 // OpenFileAndFindString opens a file and return if the given string is found or not
 func OpenFileAndFindString(filename string, expected string) bool {
 	f, err := os.Open(filename)
@@ -50,6 +78,8 @@ func OpenFileAndFindString(filename string, expected string) bool {
 		if trimmed == "" {
 			continue
 		}
+
+		// matching logic
 		if trimmed == expected {
 			return true
 		}
