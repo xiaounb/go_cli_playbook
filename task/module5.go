9c92aebc7dd166e893265f5689468cc3a844673e
Hong Xiao <jeffhongxiao@gmail.com>
2020-02-24 05:27:11 -0400

    module6 func with a comment

--- task/module5.go
+++ task/module5.go
@@ -1,6 +1,6 @@
 package main
 
-// Module5GetExampleDotCom usese th "net/http" package to send a GET request to example.com
+// Module5GetExampleDotCom uses the "net/http" package to send a GET request to example.com
 func Module5GetExampleDotCom() {
 	resp, err := http.Get("http://example.com/")
 
---
+++ task/module6.go
@@ -0,0 +1,5 @@
+package main
+
+// FunctionForModule6GoDoc needs a documentation
+func FunctionForModule6GoDoc() {
+}
